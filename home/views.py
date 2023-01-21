56df6fd773f1cf3069f3c139cb32dee44675e17c
Aiven Timptner <aiven@timptner.de>
2023-01-21 21:32:04 +0100

    Update urls

--- home/views.py
+++ home/views.py
@@ -7,8 +7,8 @@ class LandingPage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['companies'] = [
-            {'name': "Sket GmbH", 'image': "home/sket.jpg", 'url': "https://www.sket.de"},
-            {'name': "HORIBA FuelCon GmbH", 'image': "home/horiba.jpg", 'url': "https://www.horiba-fuelcon.com"},
-            {'name': "Fendt GmbH", 'image': "home/fendt.jpg", 'url': "https://www.fendt.com"},
+            {'name': "Sket GmbH", 'image': "home/sket.jpg", 'url': "https://www.sket.de/"},
+            {'name': "HORIBA FuelCon GmbH", 'image': "home/horiba.jpg", 'url': "https://www.horiba-fuelcon.com/"},
+            {'name': "Fendt GmbH", 'image': "home/fendt.jpg", 'url': "https://www.fendt.com/"},
         ]
         return context
