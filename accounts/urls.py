fdf8ce35f361a967b816ad35d11e76f438bb0bde
Aiven Timptner <aiven@timptner.de>
2023-01-24 23:41:45 +0100

    Add login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('register/', views.RegistrationView.as_view(), name='register'),
 ]
