c90ce59ab11f851064549036b1099d78928bb1e7
Aiven Timptner <aiven@timptner.de>
2023-01-24 23:42:13 +0100

    Update registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import CreateView
+from django.contrib.auth import views as auth_views
 
 from .models import User
 
 
 class RegistrationView(CreateView):
     model = User
-    fields = ['username', 'email']
+    fields = ['first_name', 'last_name', 'username', 'email', 'password']
