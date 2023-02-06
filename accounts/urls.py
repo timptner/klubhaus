dbaaaafc1fa00c6817434829fea574889a6d87a1
Aiven Timptner <aiven@timptner.de>
2023-02-06 23:32:24 +0100

    Add profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('register/', views.RegistrationView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -7,3 +10,20 @@ from .models import User
 class RegistrationView(CreateView):
     model = User
     fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+
+class ProfileView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = User
+    fields = ['first_name', 'last_name']
+    template_name = 'accounts/user_profile.html'
+    success_url = '.'
+    success_message = _("Your profile was updated successfully.")
+
+    def get_initial(self):
+        return {
+            'first_name': self.request.user.first_name,
+            'last_name': self.request.user.last_name,
+        }
+
+    def get_object(self, queryset=None):
+        return self.request.user
