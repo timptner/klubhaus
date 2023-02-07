ee4faa0fbb8721db3f4d108ce8d33f4fc54ef0d0
Aiven Timptner <aiven@timptner.de>
2023-02-07 15:18:56 +0100

    Update url names for home app

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.LandingPage.as_view(), name='landing-page'),
-    path('site_notice/', views.SiteNoticePage.as_view(), name='site-notice'),
-    path('privacy_policy/', views.PrivacyPolicyPage.as_view(), name='privacy-policy'),
+    path('', views.LandingPage.as_view(), name='landing_page'),
+    path('site_notice/', views.SiteNoticePage.as_view(), name='site_notice'),
+    path('privacy_policy/', views.PrivacyPolicyPage.as_view(), name='privacy_policy'),
 ]
