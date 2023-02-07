1919202c5c9d19194832aa73a6567e79cf90ee40
Aiven Timptner <aiven@timptner.de>
2023-02-07 21:31:09 +0100

    Enable static files

--- farafmb/settings.py
+++ farafmb/settings.py
@@ -110,9 +110,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'farafmb' / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- farafmb/urls.py
+++ farafmb/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
