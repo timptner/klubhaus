51368fa5524cbed695a162fa4653ab897caf1e80
Aiven Timptner <aiven@timptner.de>
2023-02-19 12:09:27 +0100

    Enable media uploads

--- farafmb/settings.py
+++ farafmb/settings.py
@@ -120,6 +120,15 @@ STATICFILES_DIRS = [
     BASE_DIR / 'farafmb' / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+# Media files
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- farafmb/urls.py
+++ farafmb/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('field_trips/', include('field_trips.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
