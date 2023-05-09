2f60d5b736a5c08caf6a7771283a6a5d3912b8be
Aiven Timptner <aiven@timptner.de>
2023-05-09 19:54:53 +0200

    Allow emails to include minus in username part

--- accounts/validators.py
+++ accounts/validators.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class StudentEmailValidator(validators.RegexValidator):
-    regex = r'^[\w.]+@st\.ovgu\.de$'
+    regex = r'^[\w-.]+@st\.ovgu\.de$'
     message = _(
         "Enter a valid email address. This value may contain only English letters, "
         "numbers, and dot characters. The email address must end with '@st.ovgu.de'."
