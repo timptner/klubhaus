6420b0489329b12115f179bb337107a3a0732ade
Aiven Timptner <aiven@timptner.de>
2023-05-13 10:26:35 +0200

    Add statistics

--- accounts/views.py
+++ accounts/views.py
@@ -157,6 +157,16 @@ class UserListView(PermissionRequiredMixin, ListView):
     ordering = ['first_name', 'last_name']
     context_object_name = 'account_list'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['statistics'] = {
+            'total': User.objects.count(),
+            'active': User.objects.filter(is_active=True).count(),
+            'inactive': User.objects.filter(is_active=False).count(),
+            'staff': User.objects.filter(is_staff=True).count(),
+        }
+        return context
+
 
 class UserDetailView(PermissionRequiredMixin, DetailView):
     permission_required = 'accounts.view_user'
