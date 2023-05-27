cdbcb9de337b7fa2998bfab97ad5bca24bc13c59
Aiven Timptner <aiven@timptner.de>
2023-05-27 20:40:24 +0200

    Add view to update products

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product_list'),
     path('products/add/', views.ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product_update'),
 ]
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import ProductForm
@@ -16,8 +16,18 @@ class ProductCreateView(PermissionRequiredMixin, SuccessMessageMixin, CreateView
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('merchandise:product_list')
-    success_message = "%(name)s wurde erfolgreich erstellt"
+    success_message = "%(name)s erfolgreich erstellt"
 
 
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
+
+
+class ProductUpdateView(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
+    permission_required = 'merchandise.change_product'
+    model = Product
+    form_class = ProductForm
+    success_message = "%(name)s erfolgreich aktualisiert"
+
+    def get_success_url(self):
+        return reverse_lazy('merchandise:product_detail', kwargs={'pk': self.kwargs['pk']})
