f2163057d55d3b4c1d79e6c46f9ce989c3703a8c
Aiven Timptner <aiven@timptner.de>
2023-05-28 16:59:26 +0200

    Add views to remove images

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('products/add/', views.ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('products/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/images/', views.ImageListView.as_view(), name='image_list'),
     path('products/<int:pk>/images/add/', views.ImageCreateView.as_view(), name='image_create'),
+    path('images/<int:pk>/delete/', views.ImageDeleteView.as_view(), name='image_delete'),
 ]
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .forms import ProductForm, ImageForm
@@ -33,6 +33,20 @@ class ProductUpdateView(PermissionRequiredMixin, SuccessMessageMixin, UpdateView
         return reverse_lazy('merchandise:product_detail', kwargs={'pk': self.kwargs['pk']})
 
 
+class ImageListView(PermissionRequiredMixin, ListView):
+    permission_required = 'merchandise.view_image'
+    model = Image
+
+    def get_queryset(self):
+        product = Product.objects.get(pk=self.kwargs['pk'])
+        return Image.objects.filter(product=product)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['product'] = Product.objects.get(pk=self.kwargs['pk'])
+        return context
+
+
 class ImageCreateView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
     permission_required = 'merchandise.add_image'
     model = Image
@@ -56,3 +70,19 @@ class ImageCreateView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
 
     def get_success_url(self):
         return reverse_lazy('merchandise:product_detail', kwargs={'pk': self.kwargs['pk']})
+
+
+class ImageDeleteView(PermissionRequiredMixin, SuccessMessageMixin, DeleteView):
+    permission_required = 'merchandise.delete_image'
+    model = Image
+    success_message = "%(title)s erfolgreich gelöscht"
+
+    def get_context_data(self, **kwargs):
+        image: Image = self.object
+        context = super().get_context_data(**kwargs)
+        context['product'] = image.product
+        return context
+
+    def get_success_url(self):
+        image: Image = self.object
+        return reverse_lazy('merchandise:image_list', kwargs={'pk': image.product.pk})
