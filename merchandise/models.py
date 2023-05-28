075a53cc4f82c31200244463b4628ddb1c09559a
Aiven Timptner <aiven@timptner.de>
2023-05-28 18:33:57 +0200

    Add order model

--- merchandise/models.py
+++ merchandise/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from accounts.models import User
+
 
 class Product(models.Model):
     name = models.CharField("Name", max_length=50, unique=True)
@@ -32,3 +34,40 @@ class Image(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Order(models.Model):
+    EXTRA_SMALL = 'XS'
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    EXTRA_LARGE = 'XL'
+    SIZE_CHOICES = [
+        (EXTRA_SMALL, "XS"),
+        (SMALL, "S"),
+        (MEDIUM, "M"),
+        (LARGE, "L"),
+        (EXTRA_LARGE, "XL"),
+    ]
+    PENDING = 0
+    CONFIRMED = 1
+    PAID = 2
+    READY = 3
+    COMPLETED = 4
+    STATE_CHOICES = [
+        (PENDING, "Ausstehend"),
+        (CONFIRMED, "Bestätigt"),
+        (PAID, "Bezahlt"),
+        (READY, "Abholbereit"),
+        (COMPLETED, "Abgeschlossen"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    size = models.CharField("Größe", max_length=2, choices=SIZE_CHOICES)
+    state = models.PositiveSmallIntegerField("Status", choices=STATE_CHOICES, default=PENDING)
+    created_at = models.DateTimeField("Erstellt am", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Bestellung"
+        verbose_name_plural = "Bestellungen"
+        ordering = ['created_at']
