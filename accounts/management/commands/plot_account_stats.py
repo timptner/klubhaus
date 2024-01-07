245519f06444356d63b075ed8043b43887312896
Aiven Timptner <aiven@timptner.de>
2024-01-07 22:46:02 +0100

    Use SQL query in favor of python code

--- accounts/management/commands/plot_account_stats.py
+++ accounts/management/commands/plot_account_stats.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 from pathlib import Path
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from django.db.models import DateField, Count
+from django.db.models.functions import Cast
 
 from accounts.models import User
 
@@ -17,13 +19,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         days = options['days']
-        today = datetime.utcnow().date()
-        data = {today - timedelta(days=n): 0 for n in range(days, 0, -1)}
-        users = User.objects.filter(date_joined__gt=min(data.keys()))
+        today = date.today()
+        first_date = today - timedelta(days=days)
+        queryset = User.objects.filter(
+            date_joined__date__gte=first_date,
+        ).values(
+            'date_joined__date',
+        ).annotate(count=Count('id'))
 
-        for user in users:
-            date = user.date_joined.date()
-            data[date] += 1
+        data = {item['date_joined__date']: item['count'] for item in queryset}
+
+        for n in range(days):
+            current_date = first_date + timedelta(days=n + 1)
+            if current_date not in data.keys():
+                data[current_date] = 0
 
         dates, counts = zip(*data.items())
 
