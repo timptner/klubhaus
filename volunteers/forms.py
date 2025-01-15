0a126c54418f2aa30e43d47abe26b84c1e047081
Aiven Timptner <aiven@timptner.de>
2025-01-15 22:23:08 +0100

    remove mentioning of 'freiwillige helfer' on registration

--- volunteers/forms.py
+++ volunteers/forms.py
@@ -7,45 +7,47 @@ from klubhaus.mails import PostmarkTemplate
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'date', 'teaser', 'desc', 'state', 'has_visible_counter']
+        fields = ["title", "date", "teaser", "desc", "state", "has_visible_counter"]
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'input'}),
-            'date': forms.DateInput(attrs={'class': 'input', 'type': 'date'}),
-            'teaser': forms.Textarea(attrs={'class': 'textarea', 'rows': 2}),
-            'desc': forms.Textarea(attrs={'class': 'textarea'}),
-            'state': forms.RadioSelect,
+            "title": forms.TextInput(attrs={"class": "input"}),
+            "date": forms.DateInput(attrs={"class": "input", "type": "date"}),
+            "teaser": forms.Textarea(attrs={"class": "textarea", "rows": 2}),
+            "desc": forms.Textarea(attrs={"class": "textarea"}),
+            "state": forms.RadioSelect,
         }
         help_texts = {
-            'teaser': "Maximal 500 Zeichen.",
-            'desc': "Du kannst <a href=\"https://www.markdownguide.org/cheat-sheet/\" target=\"_blank\">MarkDown</a> "
-                    "zur Formatierung nutzen.",
+            "teaser": "Maximal 500 Zeichen.",
+            "desc": 'Du kannst <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">MarkDown</a> '
+            "zur Formatierung nutzen.",
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if 'date' in self.initial:
-            self.initial['date'] = self.initial['date'].isoformat()
+        if "date" in self.initial:
+            self.initial["date"] = self.initial["date"].isoformat()
 
 
 class VolunteerForm(forms.ModelForm):
     class Meta:
         model = Volunteer
-        fields = ['comment']
+        fields = ["comment"]
         widgets = {
-            'comment': forms.Textarea(attrs={'class': 'textarea'}),
+            "comment": forms.Textarea(attrs={"class": "textarea"}),
         }
 
     def __init__(self, *args, **kwargs):
-        self.event = kwargs.pop('event')
-        self.user = kwargs.pop('user')
+        self.event = kwargs.pop("event")
+        self.user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
 
     def clean(self):
         super().clean()
 
-        is_registered = Volunteer.objects.filter(event=self.event, user=self.user).exists()
+        is_registered = Volunteer.objects.filter(
+            event=self.event, user=self.user
+        ).exists()
         if is_registered:
-            raise ValidationError("Du bist bereits als Helfer registriert.")
+            raise ValidationError("Du bist bereits registriert.")
 
     def save(self, commit=True):
         volunteer = super().save(commit=False)
@@ -59,25 +61,27 @@ class VolunteerForm(forms.ModelForm):
 
 
 class VolunteerContactForm(forms.Form):
-    message = forms.CharField(label="Nachricht", widget=forms.Textarea(attrs={'class': 'textarea'}))
+    message = forms.CharField(
+        label="Nachricht", widget=forms.Textarea(attrs={"class": "textarea"})
+    )
 
     def send_mail(self, event: Event):
         template = PostmarkTemplate()
 
         volunteers = Volunteer.objects.filter(event=event)
 
-        message = self.cleaned_data['message']
+        message = self.cleaned_data["message"]
 
         recipients = []
         payloads = []
         for volunteer in volunteers:
             recipients.append(volunteer.user.email)
             payload = {
-                'volunteer_name': volunteer.user.first_name,
-                'event_name': event.title,
-                'body': message,
+                "volunteer_name": volunteer.user.first_name,
+                "event_name": event.title,
+                "body": message,
             }
             payloads.append(payload)
 
-        errors = template.send_message_batch(recipients, payloads, 'volunteer-contact')
+        errors = template.send_message_batch(recipients, payloads, "volunteer-contact")
         return errors
--- volunteers/views.py
+++ volunteers/views.py
@@ -2,7 +2,11 @@ import csv
 
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+    UserPassesTestMixin,
+)
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
 from django.template.defaultfilters import slugify
@@ -19,22 +23,24 @@ class EventListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
-        context['my_events'] = self.request.user.volunteer_set.values_list('event__pk', flat=True)
+        context["my_events"] = self.request.user.volunteer_set.values_list(
+            "event__pk", flat=True
+        )
         return context
 
 
 class EventArchiveListView(PermissionRequiredMixin, ListView):
-    permission_required = 'volunteers.view_event'
+    permission_required = "volunteers.view_event"
     model = Event
     queryset = Event.objects.filter(state=Event.ARCHIVED)
 
 
 class EventCreateView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
-    permission_required = 'volunteers.add_event'
+    permission_required = "volunteers.add_event"
     model = Event
     form_class = EventForm
-    success_url = reverse_lazy('volunteers:event_list')
-    success_message = "\"%(title)s\" wurde erfolgreich erstellt"
+    success_url = reverse_lazy("volunteers:event_list")
+    success_message = '"%(title)s" wurde erfolgreich erstellt'
 
 
 class EventDetailView(LoginRequiredMixin, DetailView):
@@ -45,54 +51,60 @@ class EventDetailView(LoginRequiredMixin, DetailView):
         event: Event = self.object
 
         if event.state == Event.PREPARED:
-            color = 'is-info'
+            color = "is-info"
             msg = "Die Registrierung ist im Moment noch geschlossen. Schaue in ein paar Tagen noch einmal vorbei."
         elif event.state == Event.CLOSED:
-            color = 'is-danger'
+            color = "is-danger"
             msg = "Die Registrierung ist beendet und wird nicht wieder geöffnet."
         elif event.state == Event.ARCHIVED:
-            color = 'is-info'
+            color = "is-info"
             msg = "Die Veranstaltung wurde archiviert."
         elif event.volunteer_set.filter(user=self.request.user).exists():
-            color = 'is-warning'
-            msg = "Du bist für diese Veranstaltung bereits als Helfer registriert."
+            color = "is-warning"
+            msg = "Du bist für diese Veranstaltung bereits registriert."
         elif not self.request.user.phone:
-            url = reverse_lazy('accounts:profile')
-            color = 'is-danger'
-            msg = ("Bitte ergänze deine Mobilnummer in deinem "
-                   f"<a href=\"{url}\">Profil</a>, um dich anmelden zu können.")
+            url = reverse_lazy("accounts:profile")
+            color = "is-danger"
+            msg = (
+                "Bitte ergänze deine Mobilnummer in deinem "
+                f'<a href="{url}">Profil</a>, um dich anmelden zu können.'
+            )
         else:
             color = None
             msg = None
 
         if color and msg:
-            context['feedback'] = {
-                'color': color,
-                'message': msg,
+            context["feedback"] = {
+                "color": color,
+                "message": msg,
             }
 
-        context["is_volunteer"] = event.volunteer_set.filter(user=self.request.user).exists()
+        context["is_volunteer"] = event.volunteer_set.filter(
+            user=self.request.user
+        ).exists()
 
         return context
 
 
 class EventUpdateView(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
-    permission_required = 'volunteers.change_event'
+    permission_required = "volunteers.change_event"
     model = Event
     form_class = EventForm
-    success_message = "\"%(title)s\" wurde erfolgreich aktualisiert"
+    success_message = '"%(title)s" wurde erfolgreich aktualisiert'
 
     def get_success_url(self):
-        return reverse_lazy('volunteers:event_detail', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy("volunteers:event_detail", kwargs={"pk": self.kwargs["pk"]})
 
 
-class VolunteerCreateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, CreateView):
+class VolunteerCreateView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, CreateView
+):
     model = Volunteer
     form_class = VolunteerForm
-    success_message = "Du hast dich erfolgreich als freiwilliger Helfer angemeldet"
+    success_message = "Du hast dich erfolgreich angemeldet."
 
     def test_func(self):
-        event = Event.objects.get(pk=self.kwargs['pk'])
+        event = Event.objects.get(pk=self.kwargs["pk"])
         if event.state == Event.OPENED:
             return True
 
@@ -100,48 +112,48 @@ class VolunteerCreateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessag
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['event'] = Event.objects.get(pk=self.kwargs['pk'])
+        context["event"] = Event.objects.get(pk=self.kwargs["pk"])
         return context
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['event'] = Event.objects.get(pk=self.kwargs['pk'])
-        kwargs['user'] = self.request.user
+        kwargs["event"] = Event.objects.get(pk=self.kwargs["pk"])
+        kwargs["user"] = self.request.user
         return kwargs
 
     def get_success_url(self):
-        return reverse_lazy('volunteers:event_detail', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy("volunteers:event_detail", kwargs={"pk": self.kwargs["pk"]})
 
 
 class VolunteerListView(PermissionRequiredMixin, ListView):
-    permission_required = 'volunteers.view_volunteer'
+    permission_required = "volunteers.view_volunteer"
     model = Volunteer
 
     def get_queryset(self):
-        return Volunteer.objects.filter(event__pk=self.kwargs['pk'])
+        return Volunteer.objects.filter(event__pk=self.kwargs["pk"])
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
-        event = Event.objects.get(pk=self.kwargs['pk'])
-        context['event'] = event
-        context['statistics'] = {
-            'amount': event.volunteer_set.count(),
+        event = Event.objects.get(pk=self.kwargs["pk"])
+        context["event"] = event
+        context["statistics"] = {
+            "amount": event.volunteer_set.count(),
         }
         return context
 
 
 class VolunteerContactView(PermissionRequiredMixin, FormView):
-    permission_required = 'volunteers.contact_volunteer'
+    permission_required = "volunteers.contact_volunteer"
     form_class = VolunteerContactForm
-    template_name = 'volunteers/volunteer_contact.html'
+    template_name = "volunteers/volunteer_contact.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['event'] = Event.objects.get(pk=self.kwargs['pk'])
+        context["event"] = Event.objects.get(pk=self.kwargs["pk"])
         return context
 
     def form_valid(self, form):
-        event = Event.objects.get(pk=self.kwargs['pk'])
+        event = Event.objects.get(pk=self.kwargs["pk"])
 
         errors = form.send_mail(event)
 
@@ -149,33 +161,42 @@ class VolunteerContactView(PermissionRequiredMixin, FormView):
             msg = f"Es kam zu einem Problem beim Versenden der E-Mails. Bitte einen Administrator kontaktieren."
             messages.error(self.request, msg)
         else:
-            messages.success(self.request, f"Es wurden alle freiwilligen Helfer erfolgreich per E-Mails kontaktiert.")
+            messages.success(
+                self.request,
+                f"Es wurden alle freiwilligen Helfer erfolgreich per E-Mails kontaktiert.",
+            )
 
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('volunteers:volunteer_list', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy(
+            "volunteers:volunteer_list", kwargs={"pk": self.kwargs["pk"]}
+        )
 
 
-@permission_required('volunteers.view_volunteer')
+@permission_required("volunteers.view_volunteer")
 def volunteer_export(request, pk):
     event = Event.objects.get(pk=pk)
 
-    file_name = f'Freiwillige_{slugify(event.title)}.csv'
+    file_name = f"Freiwillige_{slugify(event.title)}.csv"
     response = HttpResponse(
-        content_type='text/csv',
-        headers={'Content-Disposition': f'attachment; filename="{file_name}"'},
+        content_type="text/csv",
+        headers={"Content-Disposition": f'attachment; filename="{file_name}"'},
     )
 
     writer = csv.writer(response)
-    writer.writerow(['Name', 'Mobilnummer', 'Fakultät', 'Bemerkung', 'Registrierung'])
-    for volunteer in event.volunteer_set.order_by('user__first_name', 'user__last_name'):
-        writer.writerow([
-            volunteer.user.get_full_name(),
-            f'tel:{volunteer.user.phone}',
-            volunteer.user.get_faculty_display(),
-            volunteer.comment,
-            volunteer.created_at.isoformat(),
-        ])
+    writer.writerow(["Name", "Mobilnummer", "Fakultät", "Bemerkung", "Registrierung"])
+    for volunteer in event.volunteer_set.order_by(
+        "user__first_name", "user__last_name"
+    ):
+        writer.writerow(
+            [
+                volunteer.user.get_full_name(),
+                f"tel:{volunteer.user.phone}",
+                volunteer.user.get_faculty_display(),
+                volunteer.comment,
+                volunteer.created_at.isoformat(),
+            ]
+        )
 
     return response
